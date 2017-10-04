a65d72d9e3b4bb8bd3134d4cd379d57210d6ff89
OmegaHelix <dakotaf@sasktel.net>
2017-10-04 15:31:48 -0600

    Added src File

---
+++ src/com/DakotaFisher/Class2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.DakotaFisher;
+
+/**
+ * @author fishe29d
+ *
+ */
+public class Class2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
